38897535fdff2d25e905ea7b1189d65ed38b463d
Damian Muszyński <muszynskidamian@wp.pl>
2016-06-06 19:28:07 +0200

    Update __init__.py

--- game/core/__init__.py
+++ game/core/__init__.py
@@ -1,1 +1,8 @@
-
+from .engine import Engine
+from .fight import Fight
+from .player import Player
+from .player_template import PlayerTemplate
+from .new_player import my_player
+from .error_log import wrong_choice
+from .int_input import int_input
+from .choose_skill import choose_skill
