98109427d1ee1b32a8e40748b188b164935139cf
Damian Muszyński <muszynskidamian@wp.pl>
2016-06-09 14:27:02 +0200

    Update Shazam_play.py

--- Shazam_play.py
+++ Shazam_play.py
@@ -4,3 +4,7 @@ from .game.core import Engine
 beginning = Map()
 engine1 = Engine(beginning)
 engine1.play()
+'''  alternative for testing (see also game/core/engine and game/map1/map)
+a_map = Map("welcome")
+a_game = Engine(a_map)
+a_game.play()'''  
