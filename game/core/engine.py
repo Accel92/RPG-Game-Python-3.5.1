29078b2d7f7a5bb44043e5f8a68d9c6c9ac30e44
Damian Muszyński <muszynskidamian@wp.pl>
2016-06-09 14:28:00 +0200

    Update engine.py

--- game/core/engine.py
+++ game/core/engine.py
@@ -11,6 +11,20 @@ class Engine(object):
 			try: 
 				next_scene_return = current_scene.start()
 			except AttributeError:
+				print("WATCH OUT: We go with last scene")
 				last_scene.start()
 				break
 			current_scene = self.map.next_location(next_scene_return)
+			
+'''class Engine(object):	alternative
+
+    def __init__(self, scene_map):
+        self.scene_map = scene_map
+
+    def play(self):
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene.start()
+            current_scene = self.scene_map.next_scene(next_scene_name)'''
