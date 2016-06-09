4ea77b3659fbe13ded2f6f0cc5f7e588c4252843
Damian Muszyński <muszynskidamian@wp.pl>
2016-06-09 14:34:20 +0200

    Update fight.py

--- game/core/fight.py
+++ game/core/fight.py
@@ -2,27 +2,26 @@ from sys import exit
 import random
 import time
 
+
 class Fight(object):
 	
+	def __init__(self, mob_instance):
+		self.mob = mob_instance
 		
-	def fight(self, type_of_mob, level, special_status = ""):
+	def fight(self):
 		
 		from . import my_player
-		
-		type_of_mob.capitalize()
-		mob = type_of_mob(level, special_status)
-		
-		mob_name = mob.get_name()
-		mob_hp = mob.get_hp()
+		mob_name = self.mob.get_name()
+		mob_hp = self.mob.get_hp()
 		player_name = my_player.get_name()
-		player_hp = my_player.get_health()
+		player_hp = my_player.get_hp()
 
 		print(self.mob.fight_plot)   # fight_plot is defined with mob_lvl in it
 		
 		mob_hit_chance = random.randint(0,100)
 		if self.mob.first_hit_chance_ratio >= mob_hit_chance:
 			
-			mob_dmg = mob.attack()
+			mob_dmg = self.mob.attack()
 			player_hp -= mob_dmg
 			print("")
 			print("%s's attacked you for %s dmg" %(mob_name, mob_dmg))
@@ -35,6 +34,7 @@ class Fight(object):
 		while player_hp > 0 and mob_hp > 0:
 			
 			time.sleep(1)
+			print("")
 			player_dmg = my_player.use_skill()
 			mob_hp -= player_dmg
 			print("")
@@ -44,12 +44,12 @@ class Fight(object):
 				print("%s's hp is %s now" %(mob_name, mob_hp))
 				break
 			
-			mob_dmg = mob.attack()
+			mob_dmg = self.mob.attack()
 			player_hp -= mob_dmg
 			print("%s attacked you for %s dmg" %(mob_name, mob_dmg))
 			time.sleep(1)
-			print("")
 			print("%s's hp is %s now" %(mob_name, mob_hp))
+			print("")
 			print("%s's hp is %s now" % (player_name, player_hp))	
 			time.sleep(1.5)
 		
@@ -58,6 +58,8 @@ class Fight(object):
 		else: 
 			print("You lose")
 			
-		my_player.set_stats(mob.get_exp_reward)
+		my_player.set_stats(self.mob.get_exp_reward())
 		my_player.set_skills()
-		print "Your level is now %s" % my_player.get_level()
+		print("Your level is now %s" % my_player.get_level())
+		print("END OF FIGHT HERE")
+		#return 'finished'
