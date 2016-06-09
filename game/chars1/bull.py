05908d659e3eac84beadaf319b2283eeabfec224
Damian Muszyński <muszynskidamian@wp.pl>
2016-06-09 14:31:29 +0200

    Update bull.py

--- game/chars1/bull.py
+++ game/chars1/bull.py
@@ -1,50 +1,23 @@
 from ..core import PlayerTemplate
+from ..core import Enemy
+
 from random import randint
 
-class Bull(object):
+class Warrior(Enemy):
 	
-	fight_plot = "Bull appears in front of you.\n You need to fight!"
+	fight_plot = "Enemy Warrior appears in front of you.\nYou need to fight!"
 	first_hit_chance_ratio = 30
 	
 	def __init__(self, level, special_status = ""):
-		self.__level = level
-		self.__special = special_status
-		self.__name = "Bull (lvl. %i)%s" % (self.level, self.special)
-		player_template = PlayerTemplate()
-		player_template.set_level(self.level)
-		self.__hp = player_template.get_hp()*0.7
-		self.__mana = self.__hp
-		self.__exp_reward = 100 + 50* self_level
-		
-		del player_template
-	
-	def skillset(self):
-		
-		if self.level < 3:
-			skills_dict = {
-			"1. Charge: " : self.hp * 1/6,
+		super(Warrior, self).__init__(level, special_status)
+		self._exp_reward = 100 + 50* self._level
+		#del player_template
+		if self._level < 3:
+			self._skills = {
+			"1. Charge: " : self._hp // 4,
 			}	
-			return skills_dict
-			
-		elif self.level >= 6:
-			skills_dict = {
-			"1. Charge: " : self.hp * 1/6,
-			"2. Trample: " : self.hp * 1/5,
+		elif self._level >= 6:
+			self._skills = {
+			"1. Charge: " : self._hp // 4,
+			"2. SpinSlash: " : self._hp // 3,
 			}
-			return skills_dict
-	
-	def attack(self):
-		attacks = skillset()
-		if len(attacks) = 1:
-			return attacks.value["1"]
-		else:
-			return attacks.value[randint(1:2)]
-			
-	def get_hp(self):
-		return self.__hp
-	
-	def get_name(self):
-		return self.__name
-	
-	def get_exp_reward(self):
-		return self.__exp_reward
