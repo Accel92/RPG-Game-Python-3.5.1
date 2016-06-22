d77750fe8a791b6fc77db4f490f5c6b0657897de
Damian Muszyński <muszynskidamian@wp.pl>
2016-06-22 23:24:53 +0200

    Add files via upload

---
+++ game/map1/copse.py
@@ -0,0 +1,18 @@
+import random
+import time
+
+from ..core import Fight
+from ..chars1 import Warrior
+
+class Copse(object):
+
+	def start(self):
+		print("You enter a location and encounter taunted Bull, I'm afraind")
+		print("that only thing left to do is fight")
+		my_enemy = Warrior(1, "[Boss]")
+		print("ME BE PRINTING")
+		fight1 = Fight(my_enemy)
+		fight1.fight()
+		time.sleep(2)
+		return 'third_location'
+		#return 0
