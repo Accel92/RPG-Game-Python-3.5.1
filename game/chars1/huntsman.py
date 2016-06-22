b4aaa4571d55414ca5f35097b45215519ca31965
Damian Muszyński <muszynskidamian@wp.pl>
2016-06-22 23:26:45 +0200

    Add files via upload

---
+++ game/chars1/huntsman.py
@@ -0,0 +1,27 @@
+from ..core import my_player
+
+class Huntsman(object):
+
+
+	profession_name = "Hunstsman"
+
+	def skillset(self):
+
+		player_level = my_player.get_level()
+		player_hp = my_player.get_hp()
+
+		if player_level < 3:
+			skills_dict = {
+			"1. Shot (basic): " : player_hp // 6,
+			"2. Rapid Fire: " : player_hp // 4,
+			"3. Air barrage: " : player_hp // 3
+			}
+			return skills_dict
+
+		elif player_level >= 6:
+			skills_dict = {
+			"1. Shot (basic): " : player_hp // 6,
+			"2. Rapid Fire: " : player_hp // 4,
+			"3. Air barrage: " : player_hp // 3
+			}
+			return skills_dict
---
+++ game/chars1/shadow.py
@@ -0,0 +1,28 @@
+from ..core import my_player
+
+class Shadow(object):
+
+
+	profession_name = "Shadow"
+
+	def skillset(self):
+
+		player_level = my_player.get_level()
+		player_hp = my_player.get_hp()
+
+		if player_level < 3:
+			skills_dict = {
+			"1. Slash (basic):" : player_hp  //6,
+			"1. Shadow Step: " : player_hp //4,
+			"2. Ambush: " : player_hp //3
+			}
+			return skills_dict
+
+		elif player_level >= 6:
+			skills_dict = {
+			"1. Slash (basic): " : player_hp //6,
+			"2. Shadow Step (basic): " : player_hp //4,
+			"3. Ambush: " : player_hp //3,
+			"4. Trap: " : player_hp * 3
+			}
+			return skills_dict
