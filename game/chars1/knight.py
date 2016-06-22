e73c4ac4d82547739df830ca46b9564202a2f7c6
Damian Muszyński <muszynskidamian@wp.pl>
2016-06-22 23:27:08 +0200

    Add files via upload

--- game/chars1/knight.py
+++ game/chars1/knight.py
@@ -8,19 +8,19 @@ class Knight(object):
 	def skillset(self):
 		
 		player_level = my_player.get_level()
-		player_hp = my_player.get_health()
+		player_hp = my_player.get_hp()
 		
 		if player_level < 3:
 			skills_dict = {
-			"1. Slash: " : player_hp * 1/6,
-			"2. Heavy Strike: " : player_hp * 1/5
+			"1. Slash (basic): " : player_hp // 6,
+			"2. Heavy Strike: " : player_hp // 5
 			}	
 			return skills_dict
 			
 		elif player_level >= 6:
 			skills_dict = {
-			"1. Slash: " : player_hp * 1/6,
-			"2. Heavy Slash: " : player_hp * 1/5,
+			"1. Slash (basic): " : player_hp // 6,
+			"2. Heavy Slash: " : player_hp // 5,
 			"3. Atomic Punch: " : player_hp
 			}
 			return skills_dict
--- game/chars1/mage.py
+++ game/chars1/mage.py
@@ -8,19 +8,21 @@ class Mage(object):
 	def skillset(self):
 		
 		player_level = my_player.get_level()
-		player_hp = my_player.get_health()
+		player_hp = my_player.get_hp()
 		
 		if player_level < 3:
 			skills_dict = {
-			"1. Fireball: " : player_hp * 1,
-			"2. Scorched Earth: " : player_hp * 1/5
+			"1. Arcane Bolt (basic):" : player_hp  //6,
+			"1. Fireball: " : player_hp //4,
+			"2. Scorched Earth: " : player_hp //3
 			}	
 			return skills_dict
 			
 		elif player_level >= 6:
 			skills_dict = {
-			"1. Fireball: " : player_hp * 1/6,
-			"2. Scorched Earth: " : player_hp * 1/5,
-			"3. Nuke: " : player_hp
+			"1. Arcane Bolt (basic): " : player_hp //6,
+			"2. Fireball (basic): " : player_hp //4,
+			"3. Scorched Earth: " : player_hp //3,
+			"4. Nuke: " : player_hp * 3
 			}
 			return skills_dict
