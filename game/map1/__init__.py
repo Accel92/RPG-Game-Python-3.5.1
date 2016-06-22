0c3d5401db174b9325dc0f0a303708f1b0c8d445
Damian Muszyński <muszynskidamian@wp.pl>
2016-06-22 23:25:30 +0200

    Update __init__.py

--- game/map1/__init__.py
+++ game/map1/__init__.py
@@ -1,5 +1,13 @@
+from .map import Map
+
 from .welcome import Welcome
+from .boss import Boss
+
+from .castle import Castle
+from .wizard_tower import WizardTower
+from .shadow1 import ShadowOne
+from .copse import Copse
+
 from .second_location import SecondLocation
 from .third_location import ThirdLocation
-from .boss import Boss
-from .map import Map
+
